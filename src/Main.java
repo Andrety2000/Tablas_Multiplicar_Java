28c385a87085c752c2bb2a4caa090ca77ce1f4a5
Danilo Andres Tique Briñez <dtiquebr@emeal.nttdata.com>
2022-08-07 17:18:02 -0500

    Proyecto Tabla_Base_Datos en Jva

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        JFrame frame = new JFrame(" Mi Base de Datos");
+        JTable table = new JTable();
+        DefaultTableModel tableModel = new DefaultTableModel(5, 3);
+        JButton okButton = new JButton("ok");
+
+        table.setModel(tableModel);
+
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int currentRow = table.getSelectedRow();
+                int currentColum = table.getSelectedColumn();
+                JOptionPane.showMessageDialog(null, table.getValueAt(currentRow, currentColum));
+            }
+        });
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(2, 1));
+        frame.setSize(400, 400);
+        frame.setLocationRelativeTo(null);
+        frame.add(table, 0, 0);
+        frame.add(okButton, 0, 1);
+        frame.setVisible(true);
+
+    }
+}
