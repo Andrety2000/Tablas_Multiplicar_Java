4198d3eb7c9cfa265fd406a40461ad2200075f4c
Danilo Andres Tique Briñez <dtiquebr@emeal.nttdata.com>
2022-08-07 17:29:14 -0500

    Ingrso Archivo Hola_Mundo

---
+++ HolaMundo/src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Hola, Bienvenidos a Java!!");
+        System.out.println("Hola a Todos!!!");
+
+        int edad = 25;   // => Declaración e Inicialización
+        edad = edad  + 30; // => Asignación (cambiio de valor) + operacion
+
+        String nombre;   //Declaración
+        nombre = "Danilo ANDRES tIQUE bRIÑEZ"; //Asginación
+
+
+        System.out.println("La edad es: " + edad);
+        System.out.println(nombre);
+
+    }
+}
+
